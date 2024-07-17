c44fa902bbad6725942f1b7bb2d1f273c592dcf2
Furkan SASMAZ <fsasmaz20@yahoo.com>
2024-07-17 17:33:14 +0300

    Embedding-vector-similarity-search
    A similarity search algorithm was developed with embedding vectors using the "text-ada-002" model, one of OpenAI's embedding models.

---
+++ Embedding-vector-similarity-search/find-answer.py
@@ -0,0 +1,69 @@
+from openai import OpenAI
+from pymongo import MongoClient
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import string
+
+def preprocess_text(text):
+    # Noktalama işaretlerini kaldır
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Büyük-küçük harf dönüştürme
+    text = text.lower()
+    # Gereksiz beyaz boşlukları kaldırma
+    text = ' '.join(text.split())
+    return text
+
+def find_most_similar_answer(question, collection):
+    # Kullanıcıdan gelen soruyu ön işlemden geçir
+    processed_question = preprocess_text(question)
+    
+    # Soruyu embedding vektörüne dönüştür
+    response = openai_client.embeddings.create(
+        model="embedding-model", 
+        input=[processed_question]
+    )
+    question_embedding = np.array(response.data[0].embedding).reshape(1, -1)
+    
+    # MongoDB'deki tüm embedding vektörlerini ve soru-cevap çiftlerini çek
+    all_vectors = list(collection.find({}, {'embedding_vector': 1, 'question': 1, 'answer': 1, '_id': 0}))
+    
+    # Tüm vektörlerle benzerlik hesapla
+    similarities = []
+    for vector_data in all_vectors:
+        db_vector = np.array(vector_data['embedding_vector']).reshape(1, -1)
+        similarity = cosine_similarity(question_embedding, db_vector)[0][0]
+        similarities.append((similarity, vector_data['question'], vector_data['answer']))
+    
+    # En yüksek benzerliğe sahip olanı seç
+    most_similar = max(similarities, key=lambda x: x[0])
+    return most_similar[1], most_similar[2], most_similar[0]
+
+# OpenAI istemcisini oluştur
+openai_client = OpenAI(organization="org-key", api_key="api-key")
+
+# MongoDB'ye bağlan
+mongodb_client = MongoClient('mongodb://localhost:27017/')
+db = mongodb_client['db-name']
+collection = db['collection-name']
+
+# Kullanıcıdan gelen soru
+user_question = "1.osmanlı padişahı kimdir?"
+
+# En benzer cevabı bul
+most_similar_question, answer, similarity = find_most_similar_answer(user_question, collection)
+
+if similarity > 0.9: #optional
+    print(f"Sorulan Soru: {user_question}")
+    print(f"En Benzer Soru: {most_similar_question}")
+    print(f"Cevap: {answer}")
+    print(f"Benzerlik Skoru: {similarity}")
+else:
+    print("Maalesef seni tam olarak anlayamadım. Sormuş olduğun soruyu daha farklı bir şekilde sormayı dene.")
+    print(f"En Benzer Soru: {most_similar_question}")
+    print(f"Benzerlik Skoru: {similarity}")
+    
+# MongoDB bağlantısını kapat
+mongodb_client.close()
+
+
+
---
+++ Embedding-vector-similarity-search/save-embedding.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 14:38:55 2024
+
+@author: furkan-sasmaz
+"""
+
+from openai import OpenAI
+from pymongo import MongoClient
+import json
+from datetime import datetime
+import nltk
+import string
+
+nltk.download('punkt')
+
+def preprocess_text(text):
+    # Noktalama işaretlerini kaldır
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Büyük-küçük harf dönüştürme
+    text = text.lower()
+    # Gereksiz beyaz boşlukları kaldırma
+    text = ' '.join(text.split())
+    return text
+
+def get_next_id(collection):
+    count = collection.count_documents({})
+    return count + 1
+
+def is_record_already_saved(question, collection):
+    existing_record = collection.find_one({'question': question})
+    return existing_record is not None
+
+def save_vector_to_mongodb(question, answer, text_vector, collection):
+    if is_record_already_saved(question, collection):
+        print(f'Bu soru zaten MongoDB\'ye kaydedilmiş! ({question})')
+    else:
+        next_id = get_next_id(collection)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        vector_data = {
+            'question': question,
+            'answer': answer,
+            'id': next_id,
+            'embedding_vector': text_vector,
+            'uploadDate': timestamp
+        }
+        collection.insert_one(vector_data)
+        print(f'Soru-cevap çifti başarıyla MongoDB\'ye kaydedildi! ({question})')
+
+# MongoDB'ye bağlan
+mongodb_client = MongoClient('mongodb://localhost:27017/')
+db = mongodb_client['db-name']
+collection = db['collection-name']
+
+# JSON dosyasını oku ve embedding vektörlerini oluşturup MongoDB'ye kaydet
+json_file_path = "responses2.json"
+with open(json_file_path, 'r', encoding='utf-8') as json_file:
+    qa_pairs = json.load(json_file)
+
+# OpenAI istemcisini oluştur
+openai_client = OpenAI(organization="org-key", api_key="api-key")
+
+# Her soru-cevap çifti için embedding vektörü oluştur ve MongoDB'ye kaydet
+for pair in qa_pairs:
+    question = preprocess_text(pair['question'])
+    answer = pair['answer']
+    
+    #Soruyu vektöre çevir
+    response = openai_client.embeddings.create(
+        model="embedding-model",
+        input=[question]
+    )
+    embedding_vector = response.data[0].embedding
+
+    # Vektörü MongoDB'ye kaydet
+    save_vector_to_mongodb(question, answer, embedding_vector, collection)
+
+# MongoDB bağlantısını kapat
+mongodb_client.close()
+
